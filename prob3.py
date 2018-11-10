e98d3362e936f7e5540119115f124e188b687867
triwahyuu <triwahyu34@gmail.com>
2018-11-10 23:32:26 +0900

    solved problem 3

--- prob3.py
+++ prob3.py
@@ -5,7 +5,7 @@ while ((n%2) == 0): n = int(n/2)
 
 for i in range(3, int(n**0.5) + 1, 2):
     while (n%i == 0): 
-        # print(i)
+        # print(i, end='')
         max_div = i
         n = int(n/i)
 
