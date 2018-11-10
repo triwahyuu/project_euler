05d7322be86028535fa0749b34db418ac545d65d
triwahyuu <triwahyu34@gmail.com>
2018-11-10 23:28:45 +0900

    solved problem 3

---
+++ prob3.py
@@ -0,0 +1,13 @@
+n = 600851475143
+max_div = 0
+
+while ((n%2) == 0): n = int(n/2)
+
+for i in range(3, int(n**0.5) + 1, 2):
+    while (n%i == 0): 
+        # print(i)
+        max_div = i
+        n = int(n/i)
+
+if(n > 2): max_div = n #print(n)
+print(max_div)
