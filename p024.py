4e48d9001332420cbed8a909091d84b88cf61fd5
triwahyuu <triwahyu34@gmail.com>
2018-11-13 19:21:54 +0900

    optimize p024

--- p024.py
+++ p024.py
@@ -13,14 +13,16 @@ def nth_permutation(n, nums):
     l = len(nums)
     n -= 1  # zero doesn't count, so the 1-st num is actually 0-th num here
     res = []
+    a = factorial(l-1)    # factorial of length of number left 'l'  minus 1 -> (l-1)!
 
     for i in range(l-1, -1, -1):
-        a = factorial(i)    # factorial of length of number left 'l'  minus 1 -> (l-1)!
         idx = n//a          # idx = n//(l-1)!
         n = n%a             # n = n%(l-1)!
         val = nums[idx]
+
         res.append(val)
         nums.remove(val)
+        if i != 0: a = a//i            # next factorial
     
     res = ''.join([str(i) for i in res])
     return int(res)
