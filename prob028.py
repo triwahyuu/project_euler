db34ba18809eb1eb017f80b284130691ed1899bb
triwahyuu <triwahyu34@gmail.com>
2018-11-22 04:28:58 +0900

    simplifying p028

--- prob028.py
+++ prob028.py
@@ -1,18 +1,12 @@
-## using the pattern of the spiral, when creating the spiral from 1,
-## the size of current square is always odd and the difference between 
-## two adjacent diagonal value is current size -1
+## as you can see that the top right corner of square size 'n' is 'n^2'.
+## working the way down the corner counter-clockwise, the difference of
+## adjacent corner is (n-1), so it can be calculated that the sum of the 4
+## corner is 4*n^2 - 6*(n-1). Then it can be easily calculated with sum
 
 def compute():
-    s = 1001   # size
-    n = 1   # current size
-    tot = 1
-    x = 1   # the number
-    while x < s*s:
-        d = n-1
-        while x < n*n:
-            x += d
-            tot += x
-        n += 2
+    s = 1001
+    tot = 1     # initial value of the sum, special case for 1
+    tot += sum((4*n*n - 6*(n-1)) for n in range(3, s+1, 2))
     return tot
 
 if __name__ == "__main__":
