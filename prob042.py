2acfed5a9416b19eda9bde2fd9fcf91fa1a21cb9
triwahyuu <triwahyu34@gmail.com>
2018-11-24 01:20:09 +0900

    p042 solved

--- prob042.py
+++ prob042.py
@@ -1,8 +1,19 @@
-def compute():
-    return 0
+## highest length of words in the text is 14 char, say 10 char in a word
+## use Z the value is 260, so let's use triangle number value up to 260
+triangle_number = [sum(i for i in range(1,j)) for j in range(1,25)]
 
-def is_triangle():
-    return False
+def compute():
+    txt = open("prob042_words.txt", 'r')
+    words = txt.read().split(',')
+    ans = sum(1 for w in words if is_triangle(w))
+    return ans
+
+def is_triangle(word):
+    word_val = sum((ord(i)-64) for i in word)
+    if word_val in triangle_number:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     print(compute())
