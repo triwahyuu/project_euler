ec688fadda9e19d2ef2c4bcf3f092bd602161c50
triwahyuu <triwahyu34@gmail.com>
2018-11-25 03:14:39 +0900

    simplifying algorithm

--- prob050.py
+++ prob050.py
@@ -1,19 +1,14 @@
 from euler import list_prime
-import time
 
 N = 1000000
 primes = list_prime(N)
-
 ## cache for sum of prime upto 'n' index (of prime list)
 sum_prime_upto_idx = [2, 5, 10]
 
 def compute():
-    max_cons = 0
-    for p in primes:
-        n = len_cons_prime(p)
-        if n > max_cons:
-            max_cons = n
-            ans = p
+    l = [len_cons_prime(p) for p in primes]
+    max_cons = max(l)
+    ans = primes[l.index(max_cons)]
     return ans
 
 ## returns the length of the longest consecutive prime sum of 'n'
@@ -34,26 +29,5 @@ def len_cons_prime(n):
         else:
             return m_cons
 
-## returns the length of the longest consecutive prime sum of 'n'
-def len_cons_prime1(n): 
-    # maximum number of consecutive prime sum up to n
-    tot = m_cons = 0
-    for p in primes:
-        tot += p
-        m_cons += 1
-        if tot >= n:
-            break
-
-    for i in primes:
-        if tot > n:
-            tot -= i
-            m_cons -= 1
-        elif tot < n:
-            return 0
-        else:
-            return m_cons
-
 if __name__ == "__main__":
-    t = time.time()
     print(compute())
-    print(time.time() - t)
