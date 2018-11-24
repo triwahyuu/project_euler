25903ea4a0a63766cda3e52ad946970f8fe068e1
triwahyuu <triwahyu34@gmail.com>
2018-11-25 00:10:55 +0900

    some modification for simplification

--- prob047.py
+++ prob047.py
@@ -1,48 +1,46 @@
+## using hardcoded memoization
 from euler import is_prime
-import itertools
+import itertools, time
 
-list_factor = [[0],[1],[2],[3],[2],[5]]
+list_factor = {2:[2], 3:[3]}
+seen_factor = {2,3} # used for searching
 primes = [2,3,5,7,9,11]
+seen_primes = {2,3,5,7,9,11}
 C = 4   # numbers of consecutive num
 D = 4   # numbers of distinct factors
 
 def compute():
-    for i in itertools.count(6):
-        con = 0
-        while True:
-            if len(factors(i + con)) == D:
-                con += 1
-            else:
-                break
-            
+    t = time.time()
+    con = 0
+    for i in itertools.count(4):
+        if len(factors(i)) == D:
+            con += 1
             if con == C:    # found
-                return i
+                print(time.time() - t)
+                return i-C+1
+        else:
+            con = 0
 
-## take use of memoization, some recursive
+## return distinct prime factors
+## make use of memoization and recursive
 def factors(n):
-    known_factor = len(list_factor)
-    
-    if known_factor > n:    # the factors has been found
+    if n in seen_factor:    # the factors has been found
         return list_factor[n]
-    elif known_factor < n:  # the factors before it hasn't been found
-        f = []
-        while known_factor <= n:
-            f = factors(known_factor)    # calculate the next factors
-            known_factor = len(list_factor)
-        return f
-    elif is_prime(n):       
-        list_factor.append([n])
-        primes.append(n)
+    elif is_prime(n):
+        list_factor[n] = [n]
+        seen_factor.add(n)
+        if n not in seen_primes: # if it's new prime
+            primes.append(n)
+            seen_primes.add(n)
         return [n]
     else:
         f = set()
         for d in primes:
             if n%d == 0:
-                n //= d
-                f.add(d)
-                f.update(factors(n))
+                f.update([d] + factors(n//d))
                 break
-        list_factor.append(list(f))
+        list_factor[n] = list(f)
+        seen_factor.add(n)
         return list(f)
 
 if __name__ == "__main__":
