6cf7f6975c3ad06689adbc19c2a18bc947164b5a
triwahyuu <triwahyu34@gmail.com>
2018-11-24 18:01:32 +0900

    simplifying algorithm

--- prob045.py
+++ prob045.py
@@ -1,41 +1,30 @@
 import itertools
+last_pentanum = [165, 40755]    # [n, value]
+last_hexanum = [143, 40755]
+
 def compute():
     ans = 0
     # let's look for triangle number
     for n in itertools.count(286):
-        triag = triangle(n)
+        triag = n*(n + 1)//2
         if is_hexagon(triag) and is_pentagon(triag):
             ans = triag
             break
     return ans
 
-list_pentanum = [0]     # list is faster for iterable and indexing 
-set_pentanum = set()    # set is faster for search
 def is_pentagon(val):
-    while list_pentanum[-1] < val:
-        n = len(list_pentanum)
+    while last_pentanum[1] < val:
+        n = last_pentanum[0] + 1
         y = n*(3*n - 1)//2
-        list_pentanum.append(y)
-        set_pentanum.add(y)
-    return val in set_pentanum
+        last_pentanum[:] = [n, y]
+    return val == last_pentanum[1]
 
-list_trinum = [0]
-def triangle(x):
-    while len(list_trinum) <= x:
-        n = len(list_trinum)
-        val = n*(n + 1)//2
-        list_trinum.append(val)
-    return list_trinum[x]
-
-list_hexanum = [0]
-set_hexanum = set()
 def is_hexagon(val):
-    while list_hexanum[-1] < val:
-        n = len(list_hexanum)
+    while last_hexanum[1] < val:
+        n = last_hexanum[0] + 1
         y = n*(2*n - 1)
-        list_hexanum.append(y)
-        set_hexanum.add(y)
-    return val in set_hexanum
+        last_hexanum[:] = [n, y]
+    return val == last_hexanum[1]
 
 if __name__ == "__main__":
     print(compute())
