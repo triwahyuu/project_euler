55f1441bc09ba7999f8efeab446baa6c5f5928be
triwahyuu <triwahyu34@gmail.com>
2018-11-30 17:07:45 +0900

    solved

--- prob066.py
+++ prob066.py
@@ -1,28 +1,34 @@
+## the equation is call Pell's equation
+## can be solved using continued fraction, see Prob064
+## with expansion value denoted as 'a',
+## x[n] = a*x[n-1] + x[n-2]
+## y[n] = a*y[n-1] + y[n-2]
+## min 'x' found if x[n]^2 = D*y[n]^2 + 1
+## initial value x = {1, a0}; y = {0, 1}
+## http://mathworld.wolfram.com/PellEquation.html
 import itertools
 
 def compute():
-    max_x = 0
-    for n in range(2,1000):
-        print(n)
-        x = find_min_x(n)
-        if x > max_x:
-            max_x = x
-            ans = n
-    # ans = max(range(2,8), key=find_min_x)
+    ans = max(range(2,1001), key=find_min_x)
     return ans
 
-def find_min_x(D):
-    if (D**0.5)%1 == 0: # no solution if D is perfect square
+def find_min_x(n):
+    if (n**0.5)%1 == 0: # no solution if D is perfect square
         return 0
-    for x in itertools.count(2):
-        x_sq = x*x
-        y_sq = (x_sq - 1)/D
-        if y_sq%1 != 0:
-            continue
-        print(x,y_sq)
-        if (y_sq**0.5)%1 == 0:
-            return x
-    
+    m, d = 0, 1         # initial value
+    a = a0 = int(n**0.5)
+    x = [1, a0]
+    y = [0, 1]
+    while True:
+        m = d*a - m
+        d = (n - m*m) // d
+        a = (a0 + m) // d
+
+        x.append(a*x[-1] + x[-2])
+        y.append(a*y[-1] + y[-2])
+        del x[0], y[0]
+        if x[-1]*x[-1] == (y[-1]*y[-1]*n + 1):
+            return x[-1]
+            
 if __name__ == "__main__":
-    print(find_min_x(61))
-    # print(compute())
+    print(compute())
