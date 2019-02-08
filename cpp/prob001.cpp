d9002973a143b517ba414232c2250227d02cf1cf
triwahyuu <triwahyu34@gmail.com>
2019-02-08 22:11:12 +0900

    p001 and p002 done

--- cpp/prob001.cpp
+++ cpp/prob001.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World!!" << std::endl;
+    int sum = 0;
+    for(int i = 0; i < 1000; i++)
+    {
+        if(i%3 == 0 or i%5 == 0)
+            sum += i;
+    }
+    
+    std::cout << sum << std::endl;
     return 0;
 }
---
+++ cpp/prob002.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    int fib[2] = {1,2};
+    int sum = 2, n;
+
+    while(n < 4000000){
+        n = fib[0] + fib[1];
+        fib[0] = fib[1];
+        fib[1] = n;
+        if(n%2 == 0)
+            sum += n;
+    }
+
+    std::cout << sum << std::endl;
+    return 0;
+}
