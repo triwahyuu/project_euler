a891d12cbacb36f46036b50d73e4dfde27119c3d
triwahyuu <triwahyu34@gmail.com>
2019-09-29 14:45:22 +0700

    simplifying template

--- rust/src/template.rs
+++ rust/src/template.rs
@@ -1,11 +1,9 @@
-use std::time::Instant;
-
 fn compute() -> i32{
     100
 }
 
 fn main() {
-    let now = Instant::now();
+    let now = std::time::Instant::now();
     
     let result = compute();
     println!("{:?}\tin {}ms", result, now.elapsed().as_millis());
